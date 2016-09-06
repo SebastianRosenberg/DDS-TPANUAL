5b2715a12a77284656b01203d2ecfce1af998076
FedericoStabile <federico_stabile@hotmail.com>
2016-09-05 21:37:46 -0300

    subo funciones de temporizador

--- src/main/java/tpanual/seguridad/Seguridad.java
+++ src/main/java/tpanual/seguridad/Seguridad.java
@@ -1,5 +1,29 @@
 package tpanual.seguridad;
 
-public class Seguridad {
+import tpanual.usuario.Usuario;
 
+public class Seguridad {
+	
+	//revisa estado del usuario (logueado o desloguedo)
+	//si está deslogueado, tira mensaje de error y (consultar si debe hacer algo mas)
+	//si esta logueado, prosigue con la busqueda, y al volver a temporizador, deberia cortar el tiempo y seguir con lo suyo
+	
+	public boolean Autenticacion (Usuario usuario)
+	{
+		if (estaLogueado (usuario))
+		{
+			return true;
+		}
+		else
+		{
+			System.out.println("no se pudo realizar la busqueda debido a que no se encuentra logueado");
+			return false;
+		}
+	}
+// //funcion estaLogueado, a hacer
+//	public boolean estaLogueado(Usuario usuario)
+//	{
+//		if()
+//		return true;
+//	}
 }
--- src/main/java/tpanual/temporizador/Temporizador.java
+++ src/main/java/tpanual/temporizador/Temporizador.java
@@ -1,5 +1,32 @@
 package tpanual.temporizador;
 
-public class Temporizador {
+import java.util.List;
 
+import java.time.Duration;
+import java.time.Instant;
+
+import tpanual.main.poi.PuntoDeInteres;
+import tpanual.utilitarios.Constantes;
+
+public class Temporizador {
+	
+	public Instant TiempoInicioBusqueda ()
+	{
+		Instant inicio = Instant.now();
+		return inicio;
+		
+	}
+
+	public boolean LapsoBusquedaMayor (Instant inicio)
+	{		
+	Instant fin = Instant.now();
+	Duration duration = Duration.between(inicio, fin);	
+	return (Constantes.TIEMPO_MAXIMO_CONSULTA.compareTo(duration)==1);
+	}
+
+	
 }
+
+//Utilizar el primer metodo antes de empezar la busqueda, en cuanto finaliza, llamar al segundo dentro de un if, 
+//si entra notificar al admin/loquesea
+//El segundo metodo solo verifica, la notificacion es externa
