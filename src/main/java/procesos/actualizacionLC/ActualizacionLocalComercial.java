c52e58dafb11e57af78f2cb7779780e91aaa1f9f
dipatata <aledbarreto@gmail.com>
2016-09-25 17:06:56 -0300

    ActualizacionComercial
    Subo la clase actualizacion comercial y su estructura interna

--- src/main/java/procesos/actualizacionLC/ActualizacionLocalComercial.java
+++ src/main/java/procesos/actualizacionLC/ActualizacionLocalComercial.java
@@ -1,5 +1,26 @@
 package procesos.actualizacionLC;
 
-public class ActualizacionLocalComercial {
+import java.util.List;
 
+import tpanual.main.poi.PuntoDeInteres;
+
+public class ActualizacionLocalComercial {
+	private PuntoDeInteres poi;
+	private List<String> palabrasNuevas;
+	
+	public ActualizacionLocalComercial(PuntoDeInteres poi, List<String> palabrasNuevas){
+		this.poi=poi;
+		this.palabrasNuevas=palabrasNuevas;
+	}
+	
+	
+	public PuntoDeInteres getPoi() {
+		return poi;
+	}
+	public List<String> getPalabrasNuevas() {
+		return palabrasNuevas;
+	}
+	
+	
+	
 }
