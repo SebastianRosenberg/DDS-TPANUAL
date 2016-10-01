c20e86f3da5ecffcf13b0ba7913b7eb571e73f0c
dipatata <aledbarreto@gmail.com>
2016-10-01 19:20:52 -0300

    Actualizacion en el test

--- src/test/java/tpanual/test/ActualizacionLocalesComercialesTest.java
+++ src/test/java/tpanual/test/ActualizacionLocalesComercialesTest.java
@@ -41,13 +41,9 @@ public class ActualizacionLocalesComercialesTest {
 			horario.addRangoDia(900, 1400, dia);
 		}
 		poi = PuntoDeInteresFactory.getSucursal(-34.573001D, -58.490937D, "Banco Telecom ActualizacionComercialTest", direccion, palabrasClave, servicios);
-		poi2=PuntoDeInteresFactory.getParadaDeColectivo(600, 1200, "Parada de la linea 97 ActualizacionComercialTest", direccion, palabrasClave, "97");
-		poi3=PuntoDeInteresFactory.getLocalComercial(-50D, 3000D, "Correo Argentino ActualizacionComercialTest", direccion, palabrasClave, rubro1, horario);
 		
 		//Los agrego
 		AdministradorDePoi.getInstance().agregarPoi(poi);
-		AdministradorDePoi.getInstance().agregarPoi(poi2);
-		AdministradorDePoi.getInstance().agregarPoi(poi3);
 	}
 	
 	@Test
