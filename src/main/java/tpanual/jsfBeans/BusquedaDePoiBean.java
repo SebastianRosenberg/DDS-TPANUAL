109bee1215dcb302396a63093931d4b35912a1f3
rhv044 <rhv_93@hotmail.com>
2016-11-13 19:38:50 -0300

    BusquedaDePois con JSF

--- src/main/java/tpanual/jsfBeans/BusquedaDePoiBean.java
+++ src/main/java/tpanual/jsfBeans/BusquedaDePoiBean.java
@@ -1,64 +1,59 @@
 package tpanual.jsfBeans;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 
 import tpanual.jsfcontrollers.BusquedaDePoisController;
 import tpanual.jsfcontrollers.pojos.poi.PoiPojo;
+import tpanual.jsfcontrollers.pojos.poi.SucursalBancoPojo;
+import tpanual.main.direccion.Direccion;
 
 @ManagedBean
-@ViewScoped
 public class BusquedaDePoiBean {
-	 private List<Item> items;
-	 private List<PoiPojo> pojos = new ArrayList<PoiPojo>();
-	 private List<String> datos = new ArrayList<String>();
-	 private int id=0;;
+	BusquedaDePoisController busquedaDePoisController = new BusquedaDePoisController();
+	private List<String> palabrasFiltros = new ArrayList<String>();
+	private List<PoiPojo> resultadoBusqueda = new ArrayList<PoiPojo>();
+	private String valorInputFiltros = "";
+	
+	public void agregarFiltro() {
+		this.palabrasFiltros.add(valorInputFiltros);
+	}
+
+	public void buscar() {
+		// setResultadoBusqueda(busquedaDePoisController.buscarPois(palabrasFiltros));
+		SucursalBancoPojo aux = new SucursalBancoPojo();
+		Direccion dir = new Direccion();
+		dir.setCallePrincipal("Carolina Muzilli");
+		dir.setNumero("5491");
+		aux.setDireccion(dir);
+		resultadoBusqueda.add(aux);
 	 
-	    @PostConstruct
-	    public void init() {
-	        items = new ArrayList<Item>();
-	    }
+	}
 
-	    public void add() {
-	        Item i = new Item();
-	        i.setId(this.id);
-	        id++;
-	        items.add(i);
-	    }
+	public List<String> getPalabrasFiltros() {
+		return palabrasFiltros;
+	}
 
-	    public void remove(Item item) {
-	        items.remove(item);
-	    }
+	public void setPalabrasFiltros(List<String> palabrasFiltros) {
+		this.palabrasFiltros = palabrasFiltros;
+	}
 
-	    public void save(Item item) {
-	       //buscarPoiService
-	    	//armarlista
-	    	//items.update(item);
-	    	System.out.println("items: " + items);
-	    }
+	public String getValorInputFiltros() {
+		return valorInputFiltros;
+	}
 
-	    public List<Item> getItems() {
-	        return items;
-	    }
-	    
-	    public void buscar(String datosEntrada){
-	    	//parseo el string de entrada
-	    	//List<String> datos = Arrays.asList(datosEntrada.split("\\s*,\\s*"));
-	    	//instancio la clase que llama al backend
-	    	BusquedaDePoisController buscarPoisPojos = new BusquedaDePoisController();
-	    	//hago la busqueda y lo guardo en una lista para llenar la tabla en el frontend
-	    	pojos = buscarPoisPojos.buscarPois(datos);	
-	    }
-	    
-	    public void agregarInput(String dato){
-	    	
-	    	datos.add(dato);
-	    	
-	    }
+	public void setValorInputFiltros(String valorInputFiltros) {
+		this.valorInputFiltros = valorInputFiltros;
+	}
+
+	public List<PoiPojo> getResultadoBusqueda() {
+		return resultadoBusqueda;
+	}
+
+	public void setResultadoBusqueda(List<PoiPojo> resultadoBusqueda) {
+		this.resultadoBusqueda = resultadoBusqueda;
+	}
+
 }
-
