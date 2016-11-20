a46ada874f05ae36e2d00977c5cba25abc714fdd
SebastianRosenberg <rosenberg.sebastian@gmail.com>
2016-11-20 00:06:23 -0300

    Subo cambios en la clase de Login, se agrega controller de usuarios

--- src/main/java/tpanual/jsfBeans/LoginBean.java
+++ src/main/java/tpanual/jsfBeans/LoginBean.java
@@ -5,24 +5,21 @@ import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
-import tpanual.seguridad.GestorDeUsuarios;
-import tpanual.usuario.Usuario;
-import tpanual.utilitarios.HibernateFactorySessions;
-import tpanual.utilitarios.Utilitarios;
+import tpanual.jsfcontrollers.UsuariosController;
 
 @ManagedBean
 public class LoginBean {
 	private String usuario;
 	private String contrasenia;
 	private boolean esAdministrador;
-	
+
 	public boolean getEsAdministrador() {
 		return esAdministrador;
 	}
 
 	public void setEsAdministrador(boolean esAdministrador) {
 		this.esAdministrador = esAdministrador;
-		//this.esAdministrador = true;
+		// this.esAdministrador = true;
 	}
 
 	public String getUsuario() {
@@ -48,44 +45,29 @@ public class LoginBean {
 
 	public String loginNew() {
 		String msg = "";
-		//HibernateFactorySessions hs = new HibernateFactorySessions();
-		Usuario usuarioBd;
 		Severity severity = FacesMessage.SEVERITY_INFO;
-		String resultado = new String();
-		GestorDeUsuarios gestor = GestorDeUsuarios.getInstance();
-		//usuarioBd = hs.obtenerUsuario(1);
-		//usuarioBd = Utilitarios.getHibernateFactorySessions().obtenerUsuario(3);
-		//usuarioBd = Utilitarios.getHibernateFactorySessions().obtenerUsuarioPorNombre(usuario);
+		// String resultado = new String();
+		UsuariosController userC = new UsuariosController();
+
+		boolean usuarioC = userC.buscarUsuario(usuario, contrasenia);
 		
-		//4.5.3
-		usuarioBd = gestor.buscarUsuarioPorNombre(usuario);
-		if (usuarioBd != null){
-			//this.setEsAdministrador(usuarioBd.isAdministrador());
-			if (usuario.equals(usuarioBd.getNombre()) && usuarioBd.login(contrasenia)) {
-				msg = "Usuario " + usuario + " autorizado";
-				resultado = "Logueado";
-				this.mensajeAPantalla(msg, severity);
-				this.setEsAdministrador(usuarioBd.isAdministrador());
-			} else {
-				msg = "Usuario no autorizado";
-				resultado = "No Logueado";
-				severity = FacesMessage.SEVERITY_ERROR;
-				this.mensajeAPantalla(msg, severity);
-			}
-			//FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, msg, null));
-		}else{
-			msg = "Usuario inexistente";
-			resultado = "No Logueado";
+		if (usuarioC) {
+			esAdministrador = userC.esAdministrador();;
+			msg = "Usuario " + usuario + " autorizado";
+			this.mensajeAPantalla(msg, severity);
+			return "Logueado";
+		} else {
+			msg = "Usuario no autorizado";
 			severity = FacesMessage.SEVERITY_ERROR;
 			this.mensajeAPantalla(msg, severity);
+			return "No Logueado";
 		}
-		return resultado;
 	}
-	
-	private void mensajeAPantalla(String mensaje,Severity severidad){
-		
+
+	private void mensajeAPantalla(String mensaje, Severity severidad) {
+
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severidad, mensaje, null));
-		
+
 	}
 
 }
