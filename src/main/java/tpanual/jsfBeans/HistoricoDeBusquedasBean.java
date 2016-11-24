1f2ff0700593a98be7ae7eead5b4b4ed4a26bea7
SebastianRosenberg <rosenberg.sebastian@gmail.com>
2016-11-24 01:48:25 -0300

    Se agrega redirección en las vistas cuando el usuario no está logueado

--- src/main/java/tpanual/jsfBeans/HistoricoDeBusquedasBean.java
+++ src/main/java/tpanual/jsfBeans/HistoricoDeBusquedasBean.java
@@ -1,14 +1,21 @@
 package tpanual.jsfBeans;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.faces.FacesException;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import org.joda.time.DateTime;
 
 import administrador.AdministradorDePoi;
 import tpanual.jsfcontrollers.pojos.busqueda.BusquedaPojo;
 import tpanual.seguridad.GestorDeUsuarios;
+import tpanual.jsfcontrollers.BusquedaDePoisController;
 import tpanual.jsfcontrollers.HistorialDeBusquedasController;
 
 @ManagedBean
@@ -51,28 +58,34 @@ public class HistoricoDeBusquedasBean {
 	}
 
 	public void buscar() {
-		DateTime fechaDesdeJoda = null;
-		DateTime fechaHastaJoda = null;
-		infoPOI.clear();
-		if(fechaDesde != null)
-			fechaDesdeJoda = new DateTime(fechaDesde);
-		if (fechaHasta != null)
-			fechaHastaJoda= new DateTime(fechaHasta);
-		/*BusquedaPojo unaBusqueda = new BusquedaPojo();
-		unaBusqueda.setFecha(new DateTime());
-		unaBusqueda.setTotal(3);
-		unaBusqueda.setParametros(new String[] { "uno", "dos" });
-		unaBusqueda.setUsuario(GestorDeUsuarios.getInstance().crearTerminalActivo("pedro"));
-		unaBusqueda.setIds(new int[] { 1, 2, 3 });
-		busquedas.add(unaBusqueda);*/
-		busquedas = busquedasController.getHistorialBusquedas(fechaDesdeJoda,fechaHastaJoda, nombreUsuario);
+
+		if (BusquedaDePoisController.estasLogueado()) {
+			DateTime fechaDesdeJoda = null;
+			DateTime fechaHastaJoda = null;
+			infoPOI.clear();
+			if (fechaDesde != null)
+				fechaDesdeJoda = new DateTime(fechaDesde);
+			if (fechaHasta != null)
+				fechaHastaJoda = new DateTime(fechaHasta);
+			/*
+			 * BusquedaPojo unaBusqueda = new BusquedaPojo();
+			 * unaBusqueda.setFecha(new DateTime()); unaBusqueda.setTotal(3);
+			 * unaBusqueda.setParametros(new String[] { "uno", "dos" });
+			 * unaBusqueda.setUsuario(GestorDeUsuarios.getInstance().
+			 * crearTerminalActivo("pedro")); unaBusqueda.setIds(new int[] { 1,
+			 * 2, 3 }); busquedas.add(unaBusqueda);
+			 */
+			busquedas = busquedasController.getHistorialBusquedas(fechaDesdeJoda, fechaHastaJoda, nombreUsuario);
+		} else {
+			this.redirectToPage("/login.xhtml");
+		}
 	}
 
 	public void masInfo(int[] ids) {
 		infoPOI.clear();
 		for (int i = 0; i < ids.length; i++) {
 			infoPOI.add(AdministradorDePoi.getInstance().obtenerPoiPorId(ids[i]).getNombre());
-			//infoPOI.add(String.valueOf(i));
+			// infoPOI.add(String.valueOf(i));
 		}
 	}
 
@@ -90,6 +103,19 @@ public class HistoricoDeBusquedasBean {
 
 	public void setInfoPOI(List<String> infoPOI) {
 		this.infoPOI = infoPOI;
+	}
+
+	private void redirectToPage(String toUrl) {
+		try {
+			FacesContext ctx = FacesContext.getCurrentInstance();
+
+			ExternalContext extContext = ctx.getExternalContext();
+			String url = extContext.encodeActionURL(ctx.getApplication().getViewHandler().getActionURL(ctx, toUrl));
+
+			extContext.redirect(url);
+		} catch (IOException e) {
+			throw new FacesException(e);
+		}
 	}
 
 }
