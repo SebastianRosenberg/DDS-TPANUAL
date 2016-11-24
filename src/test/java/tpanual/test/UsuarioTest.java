57c5348ed116236f93d3ac8c9270ddee1186bfd7
millanes <millanes92@gmail.com>
2016-11-24 02:14:29 -0300

    Arreglo tests de usuarios

--- src/test/java/tpanual/test/UsuarioTest.java
+++ src/test/java/tpanual/test/UsuarioTest.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import administrador.Mapa;
 import tpanual.factory.PuntoDeInteresFactory;
+import tpanual.jsfcontrollers.BusquedaDePoisController;
 import tpanual.main.Servicio;
 import tpanual.main.direccion.Direccion;
 import tpanual.main.poi.PuntoDeInteres;
@@ -21,74 +23,54 @@ import tpanual.usuario.Usuario;
 public class UsuarioTest {
 
 	static GestorDeUsuarios gestor = GestorDeUsuarios.getInstance();
-	
-	@Test
-	public void administradorAgregarPoiTest(){
+	static PuntoDeInteres poi;
+
+	@BeforeClass
+	public static void initialize() {
+		/* Set up Poi */
+		Direccion direccion = new Direccion.DireccionBuilder().callePrincipal("Rivadavia").numero("7777").barrio("Flores")
+				.crearDireccion();
+		List<String> palabras = new ArrayList<String>();
+		palabras.add("Usuario Test");
+
+		List<Servicio> servicios = Servicio.getListaServicios("ServicioUsuario1", "ServicioUsuario2");
+
+		poi = PuntoDeInteresFactory.getCGP(2500D, 3200D, "CGP Usuario Test", direccion, palabras, servicios, 25);
+
+		/* Seteo usuarios */
+		gestor.crearAdministrador("AdministradorTest", "esuntest@gmail.com", "1234cinco");
 		
+		gestor.crearTerminalActivo("UsuarioActivoTest");
 		
-		
-		/*Set up Poi*/
-		Direccion direccion=new Direccion.DireccionBuilder().callePrincipal("Pueyrredon").numero("545").barrio("Once").crearDireccion();
-				List<String> palabras=new ArrayList<String>();
-				palabras.add("Servicio de cafeteria");
-				palabras.add("Mala Atencion");
-		
-		List<String> palabras2=new ArrayList<String>();
-		palabras2.add("CGP");
-		palabras2.add("Zona peligrosa");
-		
-		List<Servicio> servicios=Servicio.getListaServicios("Registro Civil", "Denuncias", "Pensiones");
-		
-		PuntoDeInteres poi=PuntoDeInteresFactory.getCGP(2500D, 3200D, "GCP Comuna 1", direccion, palabras2, servicios, 25);
-		
-		
-		PuntoDeInteres poi1=PuntoDeInteresFactory.getParadaDeColectivo(600, 1200, "Parada de la linea ciento catorce", direccion, palabras2, "114");
-		
-		/*Seteo usuario*/
-		Usuario nuevoUsuario = gestor.crearAdministrador("UserTest", "esuntest@gmail.com","1234cinco");
-		
-		/*Tareas del usuario*/
-		nuevoUsuario.agregarPoi(poi);
-		nuevoUsuario.agregarPoi(poi1);
-		
-		/*asigno a una lista el resultado de la b�squeda*/
-		List<PuntoDeInteres> lista= nuevoUsuario.busquedaDePuntosDeInteres("", true);
-		
-		assertTrue(lista.contains(poi));
+		gestor.crearTerminalNoActivo("UsuarioNoActivoTest");
+
 	}
-	
+
 	@Test
-	public void administradorEliminarPoiTest(){
-				
-		Usuario nuevoUsuario = gestor.crearAdministrador("UserTest", "esuntest@gmail.com","1234cinco");
+	public void administradorAgregarPoiTest() {
+
+		Usuario admin = gestor.buscarUsuarioPorNombre("AdministradorTest");
 		
-		List<PuntoDeInteres> lista=nuevoUsuario.busquedaDePuntosDeInteres("Parada de la linea ciento catorce", true);
-		Iterator<PuntoDeInteres> i = lista.iterator();
-		
-		PuntoDeInteres poi1 = null;
-		i = lista.iterator();
-		while(i.hasNext()){	
-			PuntoDeInteres n=i.next();
-			nuevoUsuario.eliminarPoi(n);
-			poi1 = n;
-		}
-		
-		assertFalse(lista.contains(poi1.getId()));
+		/* Tarea del admin */
+		admin.agregarPoi(poi);
+
+		/* Busco el poi que acabo de agregar */
+		assertTrue(admin.busquedaDePuntosDeInteres("cgp usuario test", true).size()>0);
 	}
-	
+
 	@Test
-	public void usuarioTerminalActivoBusquedaTest(){
-		
-		Usuario usuarioTerminalActivo = gestor.crearTerminalActivo("UserTest");
-		assertTrue(usuarioTerminalActivo.busquedaDePuntosDeInteres("", true)!=null);
+	public void usuarioTerminalActivoBusquedaTest() {
+
+		Usuario usuarioTerminalActivo = gestor.buscarUsuarioPorNombre("UsuarioActivoTest");
+		assertTrue(usuarioTerminalActivo.busquedaDePuntosDeInteres("cgp usuario test", true).size()>0);
 	}
-	
+
 	@Test
-	public void usuarioTerminalNoActivoBusquedaTest(){
-		
-		Usuario usuarioTerminalNoActivo = gestor.crearTerminalNoActivo("UserTest");
-				
-		assertTrue(usuarioTerminalNoActivo.busquedaDePuntosDeInteres ("", true)==null);
+	public void usuarioTerminalNoActivoBusquedaTest() {
+
+		Usuario usuarioTerminalNoActivo = gestor.buscarUsuarioPorNombre("UsuarioNoActivoTest");
+
+		assertTrue(usuarioTerminalNoActivo.busquedaDePuntosDeInteres("cgp usuario test", true) == null);
 	}
-	
+
 }
